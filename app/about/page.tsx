fc9ee2a29156253881f1490a3b6f9cba92d4c99d
hoanganhhththt <66316469+hoanganhhththt@users.noreply.github.com>
2023-12-12 10:57:02 +0700

    first commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from '../page.module.css'
 
-export default function Home() {
+export default function AboutPage() {
   return (
     <main className={styles.main}>
-      <div className={styles.title}>Homepage</div>
+      <div className={styles.title}>Aboutpage</div>
       <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores aspernatur aut reiciendis quaerat cupiditate natus porro nobis sit, quae beatae inventore quia iure minima distinctio dolorum culpa mollitia aperiam sint.</p>
       <p className={styles.text}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe voluptatibus repudiandae eius, officiis mollitia labore, provident libero quis eaque voluptate vel exercitationem sunt ea reiciendis aliquam tenetur! Dolor, doloremque! Alias?</p>
     </main>
